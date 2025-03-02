d205ce97facc77b24aa09ab1fc8ecc93514a0aba
Sanjal28 <slsanjal28@gmail.com>
2025-03-02 22:27:51 +0530

    Explored get,post and delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello from  server");
+
+app.get("/user", (req, res) => {
+  res.send("This handles the GET request for /user");
 });
-app.get("/test", (req, res) => {
+app.post("/user", (req, res) => {
+  res.send("This handles the POST request for /user");
+});
+app.use("/hello/2", (req, res) => {
+  res.send("Hello");
+});
+app.use("/hello", (req, res) => {
   res.send("Testing Phase....");
 });
-app.get("/hello", (req, res) => {
-  res.send("Hello");
+
+app.use("/", (req, res) => {
+  res.send("Hello from  server");
 });
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
